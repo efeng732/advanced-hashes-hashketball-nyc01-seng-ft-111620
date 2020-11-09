2ab82faaf6fe1960be4a58aad8e2a30527bc2bda
Erwin Feng <efeng732@gmail.com>
2020-11-09 22:47:42 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -171,8 +171,8 @@ end
 def player_numbers (team_name)
   game_hash.each do |team, team_info|
     if team_info[:team_name] == team_name
-      team_info[:players].map do |jersey_number|
-        jersey_number[:number]
+      team_info[:players].map do |player, jersey|
+        jersey[:number]
       end 
     end 
   end 
