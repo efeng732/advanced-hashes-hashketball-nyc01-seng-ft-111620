47b6ad5b92efd4b154cfa79bade28fe52c23cee7
Erwin Feng <efeng732@gmail.com>
2020-11-09 22:21:44 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -132,7 +132,7 @@ def num_points_scored(player_name)
   game_hash.each do |team, team_info|
     team_info[:players].each do |player|
       if player == player_name 
-        return players[:points]
+        return player[:points]
       end 
 end
 end 
