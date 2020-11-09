b790afb8de7a32ac38c0662898f9c5ec6b1d249f
Erwin Feng <efeng732@gmail.com>
2020-11-09 22:17:50 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -130,7 +130,8 @@ end
 
 def num_points_scored(player_name)
   game_hash.each do |team, team_info|
-    team_info[:players].each do |
+    team_info[:players].each do |players, stats|
+      
 
 
 
